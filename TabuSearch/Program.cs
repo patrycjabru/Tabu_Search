2f6210c2005d1b7fa19cc02328197972c044f259
Patrycja Bruździńska <patrycja.bruzdzinska@gmail.com>
2021-01-21 10:33:15 +0100

    fix images

--- TabuSearch/Program.cs
+++ TabuSearch/Program.cs
@@ -21,21 +21,17 @@ namespace TabuSearch
 
             var algorithmRuns = 50;
             var timeLimitInSeconds = 120;
-            var searchIterations = new List<int>() {900, 1100, 1300, 1500};
+            var searchIterations = new List<int>() { 900, 1100, 1300, 1500 };
             var multipleResultsIterations = runner.RunWithMultipleIterations(algorithmRuns, problem, search, searchIterations, 20, 4, timeLimitInSeconds);
             plotGenerator.GenerateMultipleBoxAndWhiskers(multipleResultsIterations, searchIterations, "Solution energy for different values of search iterations", "multiple results iterations");
 
-            //var minTabus = new List<int>(){10, 20, 30, 40, 50};
-            //var multipleResultsMinTabu = runner.RunWithMultipleMinTabu(algorithmRuns, problem, search, 100, minTabus, 4, timeLimitInSeconds);
+            //var minTabus = new List<int>() { 10, 20, 30, 40, 50 };
+            //var multipleResultsMinTabu = runner.RunWithMultipleMinTabu(algorithmRuns, problem, search, 100, minTabus, 4, 1);
             //plotGenerator.GenerateMultipleBoxAndWhiskers(multipleResultsMinTabu, minTabus, "Solution energy for different values of min tabu", "multiple results min tabu");
 
-            //var extraTabus = new List<int>(){5, 10, 15, 20, 25};
-            //var multipleResultsExtraTabu = runner.RunWithMultipleExtraTabu(algorithmRuns, problem, search, 100, 20, extraTabus, timeLimitInSeconds);
+            //var extraTabus = new List<int>() { 5, 10, 15, 20, 25 };
+            //var multipleResultsExtraTabu = runner.RunWithMultipleExtraTabu(algorithmRuns, problem, search, 100, 20, extraTabus, 1);
             //plotGenerator.GenerateMultipleBoxAndWhiskers(multipleResultsExtraTabu, extraTabus, "Solution energy for different values of extra tabu", "multiple results extra tabu");
-
-
-
-
         }
     }
 }
